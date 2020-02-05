9e2c218f201abbb307e767e364138683b11a3c30
Zhi-BeYourHero <763378665@qq.com>
2020-02-05 11:18:05 +0800

    添加了登录功能

---
+++ src/main/java/com/zhi/controller/GoodsController.java
@@ -0,0 +1,26 @@
+package com.zhi.controller;
+
+import com.zhi.domain.MiaoshaUser;
+import com.zhi.redis.RedisService;
+import com.zhi.service.MiaoshaUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+    @RequestMapping("/to_list")
+    public String list(Model model,MiaoshaUser user) {
+    	model.addAttribute("user", user);
+        return "goods_list";
+    }
+}
---
+++ src/main/java/com/zhi/validator/IsMobileValidator.java
@@ -0,0 +1,35 @@
+package com.zhi.validator;
+
+import com.zhi.util.ValidatorUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * @author WenZhi Luo
+ * @version 1.0
+ * @date 2020/2/4 17:03
+ */
+public class IsMobileValidator implements ConstraintValidator<IsMobile, String> {
+
+    private boolean required = false;
+
+    @Override
+    public void initialize(IsMobile isMobile) {
+        required = isMobile.required();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if(required) {
+            return ValidatorUtil.isMobile(value);
+        }else {
+            if(StringUtils.isEmpty(value)) {
+                return true;
+            }else {
+                return ValidatorUtil.isMobile(value);
+            }
+        }
+    }
+}
